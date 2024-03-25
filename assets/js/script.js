64dafef5a5f6566082c4b27c94d668ce320b5506
rohansharma-developer <rohansharma.developer@skiff.com>
2024-03-25 09:37:20 +0530

    hotfixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,3 +87,21 @@ window.addEventListener('scroll', function() {
 
 
 
+
+
+var b = document.getElementsByTagName("main")[0];  
+
+b.addEventListener("mousemove", function(event) {
+  parallaxed(event);
+
+});
+
+function parallaxed(e) {
+      var amountMovedX = (e.clientX * 0.2 / 8);
+      var amountMovedY = (e.clientY * 0.2 / 8);
+      var x = document.getElementsByClassName("parallaxed");
+      var i;
+      for (i = 0; i < x.length; i++) {
+        x[i].style.transform='translate(' + amountMovedX + 'px,' + amountMovedY + 'px)'
+      }
+}
